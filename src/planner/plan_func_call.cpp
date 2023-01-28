314f552acdde28c444c086c3792d9b2f18e5c080
zhixuanevelynwu <zw1887@nyu.edu>
2023-01-28 15:33:56 -0500

    Done with p0

--- src/planner/plan_func_call.cpp
+++ src/planner/plan_func_call.cpp
@@ -39,13 +39,16 @@ auto Planner::GetFuncCallFromFactory(const std::string &func_name, std::vector<A
   // 1. check if the parsed function name is "lower" or "upper".
   // 2. verify the number of args (should be 1), refer to the test cases for when you should throw an `Excepetion`.
   // 3. return a `StringExpression` std::shared_ptr.
-  if ((func_name == "lower" || func_name == "upper") && args.size() == 1) {
+  if (func_name == "lower" || func_name == "upper") {
+    if (args.size() != 1) {
+      throw bustub::NotImplementedException("expect 1 argument");
+    }
     if (func_name == "upper") {
       return std::make_shared<StringExpression>(args[0], StringExpressionType::Upper);
     }
     return std::make_shared<StringExpression>(args[0], StringExpressionType::Lower);
   }
-  return nullptr;
+  throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
 }
 
 }  // namespace bustub
